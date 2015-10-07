2ca4e084d676a0a03ff88f082069cb27f1a63b50
Chris Loew <cloew123@gmail.com>
2015-10-07 13:54:39 -0600

    Added basic kao input

---
+++ dist/kao_input.js
@@ -0,0 +1,40 @@
+$traceurRuntime.ModuleStore.getAnonymousModule(function() {
+  "use strict";
+  angular.module("kao.input", []).directive("keyTrap", function() {
+    return function(scope, elem) {
+      elem.bind("keydown", function(event) {
+        scope.$broadcast("keydown", event);
+      });
+    };
+  }).directive("onEnterKey", function($timeout) {
+    return function(scope, element, attrs) {
+      scope.$on("keydown", function(msg, event) {
+        if (event.which === 13) {
+          scope.$evalAsync(attrs.onEnterKey, {"event": event});
+          event.preventDefault();
+        }
+      });
+    };
+  }).directive("onUpKey", function($timeout) {
+    return function(scope, element, attrs) {
+      scope.$on("keydown", function(msg, event) {
+        if (event.which === 38) {
+          scope.$evalAsync(attrs.onUpKey, {"event": event});
+          event.preventDefault();
+        }
+      });
+    };
+  }).directive("onDownKey", function($timeout) {
+    return function(scope, element, attrs) {
+      scope.$on("keydown", function(msg, event) {
+        if (event.which === 40) {
+          scope.$evalAsync(attrs.onDownKey, {"event": event});
+          event.preventDefault();
+        }
+      });
+    };
+  });
+  return {};
+});
+
+//# sourceMappingURL=kao_input.map
